03572539d63c703602d3a3af7d6deedd2ee8e2d8
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-10 16:56:01 +0530

    Completed the project

--- react-facts/src/App.jsx
+++ react-facts/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header.jsx';
+import MainContent from "./components/MainContent/MainContent.jsx";
 
 function App(){
     return(
         <>
             <Header />
+            <MainContent/>
         </>
     );
 }

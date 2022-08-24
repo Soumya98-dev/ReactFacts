a4d544f923869358c0fd8f9f36a3fbb760b9695a
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-25 01:12:26 +0530

    Added darkMode toggler

--- react-facts/src/App.jsx
+++ react-facts/src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Header from './components/Header/Header.jsx';
+import React from "react";
+import Header from "./components/Header/Header.jsx";
 import MainContent from "./components/MainContent/MainContent.jsx";
+import "./App.css";
 
-function App(){
-    return(
-        <>
-            <Header />
-            <MainContent/>
-        </>
-    );
+function App() {
+  // *** STATE FOR DARK MODE
+  const [darkMode, setDarkMode] = React.useState(true);
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+  return (
+    <div className="container">
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <MainContent darkMode={darkMode} />
+    </div>
+  );
 }
 
 export default App;
--- react-facts/src/components/Header/Header.jsx
+++ react-facts/src/components/Header/Header.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import './Header.css';
+import "./Header.css";
 
-function Header() {
-    return (
-        <>
-            <nav>
-                <div className="title">
-                    <img src="../public/react-js-logo.png" alt="React logo"/>
-                    <h1>ReactFacts</h1>
-                </div>
-                <h3>React Course - Project 1</h3>
-            </nav>
-        </>
-    )
+function Header(props) {
+  return (
+    <nav className={props.darkMode ? "dark" : ""}>
+      <div className="logo">
+        <img src="../public/react-js-logo.png" alt="React logo" />
+        <h1>ReactFacts</h1>
+      </div>
+      <div className="toggler">
+        <p className="toggler-light">Light</p>
+        <div onClick={props.toggleDarkMode} className="toggler-slider">
+          <div className="toggler-circle"></div>
+        </div>
+        <p className="toggler-dark">Dark</p>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
